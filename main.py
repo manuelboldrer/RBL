2dbcf44a94159235fd5c4e00c446e410678fa22f
Manuel Boldrer <95948066+manuelboldrer@users.noreply.github.com>
2023-09-06 09:28:24 +0200

    minor

--- main.py
+++ main.py
@@ -1,45 +1,45 @@
 import random
 import argparse
 from simulate import simulate
-N = 5
+from simulate import check_parameters
+
+N = 20                                 # Number of robots.
 
 parameters = {
-        "R_circle": 3,                # If greater than 0, robots in a circle (radius of the circle).
+        "R_circle": 0,                # If greater than 0, robots in a circle (radius of the circle).
                                       # If equal to 0, robots in a random room.
-        "Xplot": 5,                   #x-axis plot
-        "Yplot": 5,                   #y-axis plot
-        "radius": 1,                # Half of the sensing radius: dimension of the cells.
-        "xlim": (0, 5),               # Random room dimensions on the X-axis.
-        "ylim": (0, 5),               # Random room dimensions on the Y-axis.
+        "Xplot": 5,                   # x-axis plot
+        "Yplot": 5,                   # y-axis plot
+        "radius": 1.5,                # Half of the sensing radius: dimension of the cells r_{s,i}=r_{s}.
+        "xlim": (0, 3),             # Random room dimensions on the X-axis.
+        "ylim": (0, 3),             # Random room dimensions on the Y-axis.
         "N": N,                       # Number of robots.
         "num_steps": 3000,            # Number of simulation steps.
-        "dx": 0.1,                    # Space discretization.
-        "dt": 0.033,                  # Time discretization.
-        "d1": 0.1,                    # d1 eq. (8).
-        "d2": 0.45,                    # d2 eq. (8).
-        "d3": 0.1,                    # d3 eq. (9).
-        "d4": 0.45,                    # d4 eq. (9).
-        "R_gauss_min": 0.05,          # Minimum value for spreading factor rho.
-        "R_gaussianD": [random.uniform(0.2, 0.2) for _ in range(N)],  # Desired spreading factor \rho^D.
+        "dx": 0.1,                    # Space discretization. [It introduce an approximation. The lower the better, but it cost computation]
+        "dt": 0.033,                  # Time discretization. [do not change]
+        "d1": 0.2,                    # d1 eq. (8). [do not change]
+        "d3": 0.2,                    # d3 eq. (9). [do not change]
+        "R_gauss_min": 0.05,          # Minimum value for spreading factor rho. [do not change]
+        "R_gaussianD": [0.25]*N,       # Desired spreading factor \rho^D.
         "size": [random.uniform(0.15, 0.15) for _ in range(N)],        # Robots encumbrance \delta
-        "k": [random.uniform(5, 5) for _ in range(N)],          # Control parameter k_p
+        "k": [6]*N,                   # Control parameter k_p
         "flag_plot": 0,
         "write_file": 0
-
 }
 
-
-parser = argparse.ArgumentParser(description="HLB.")
-parser.add_argument("-render", action="store_true", help="Activate rendering")
-parser.add_argument("-writefile", action="store_true", help="Write to a file")
+parser = argparse.ArgumentParser(description= "HLB.")
+parser.add_argument("-render", action="store_true", help = "Activate rendering")
+parser.add_argument("-writefile", action="store_true", help = "Write to a file")
 args = parser.parse_args()
 if args.render:
     parameters["flag_plot"] = 1
 if args.writefile:
     parameters["write_file"] = 1
 
-repetitions = 10
-for h in range(1,repetitions):
+repetitions = 100
+check_parameters(parameters)
+
+for h in range(1 , repetitions):
     print("Number of repetition", h, "of", repetitions)
     simulate( h, parameters)
 
--- simulate.py
+++ simulate.py
@@ -4,11 +4,18 @@ import matplotlib.pyplot as plt
 from RobotInit import RobotsInit
 from Lloydbasedalgorithm import LloydBasedAlgorithm
 from plot_utils import plot_circle, plot_line  
+import time
 
+def check_parameters(P):
+    for j in range(P["N"]):
+        if (((P["radius"]-P["R_gaussianD"][j])*math.exp(2*P["radius"]/P["R_gaussianD"][j])+P["radius"]+P["R_gaussianD"][j])/(math.exp(2*P["radius"]/P["R_gaussianD"][j])-1)) < 3*P["size"][j]:
+            print("parameters not selected correctly. It may not converge.")
+        if P["d3"] >  (((P["radius"]-P["R_gaussianD"][j])*math.exp(2*P["radius"]/P["R_gaussianD"][j])+P["radius"]+P["R_gaussianD"][j])/(math.exp(2*P["radius"]/P["R_gaussianD"][j])-1)) - 3*P["size"][j] or P["d1"] > (((P["radius"]-P["R_gaussianD"][j])*math.exp(2*P["radius"]/P["R_gaussianD"][j])+P["radius"]+P["R_gaussianD"][j])/(math.exp(2*P["radius"]/P["R_gaussianD"][j])-1)) - 3*P["size"][j]:
+            print("parameters not selected correctly. It may not converge.")
 
 def simulate( h, P ):
     #Initialize variables
-    tmp = 0
+    tmp  = 0
     c1 = [0,0] * P["N"]  #actual centroids 
     c2 = [0,0] * P["N"]  #virtual centroids (without neighbours)
     c1_no_rotation = [0,0] * P["N"]  # \bar p = e, centroids  
@@ -29,6 +36,7 @@ def simulate( h, P ):
         Lloyd[j]  = LloydBasedAlgorithm(Robots.positions[j], P["radius"], P["dx"], P["k"][j], P["size"][j], np.delete(P["size"], j, axis=0), P["dt"])
         Lloyd_virtual[j] = LloydBasedAlgorithm(Robots.positions[j], P["radius"], P["dx"], P["k"][j], P["size"][j], np.delete(P["size"], j, axis=0), P["dt"])
 
+
     while sum(flag) < P["N"] and step <P["num_steps"]: # until all robots reach their goal or the number of steps is reached            
         if P["flag_plot"] == 1:
             plt.xlim([-P["Xplot"],P["Xplot"]])
@@ -37,6 +45,7 @@ def simulate( h, P ):
             plt.clf()
         step= step+1
         for j in range(P["N"]):   #for each robot
+            start = time.time()
             position_other_robots=np.delete(Robots.positions, j, axis=0)
             Lloyd[j].aggregate(position_other_robots , R_gaussian[j], Robots.destinations[j])   
             Lloyd_virtual[j].aggregate(position_other_robots, R_gaussian[j], goal[j])
@@ -48,22 +57,23 @@ def simulate( h, P ):
             u = Lloyd[j].compute_control()
             if np.sqrt(u[0]**2+u[1]**2) >tmp:
                 tmp = np.sqrt(u[0]**2+u[1]**2)
-                
+            
             #Apply the Heuristic inputs to modify Rgaussian and Robots.destinations on the basis of c1 and c2          
-
             #equation (8)
-            if abs(np.linalg.norm(np.array(c1[j]) - np.array(c2[j]))) > P["d2"] and np.linalg.norm(np.array(current_position[j]) - np.array(c1[j])) < P["d1"]:
+            d2 = 3*P["size"][j]
+            d4 = d2
+            if abs(np.linalg.norm(np.array(c1[j]) - np.array(c2[j]))) > d2 and np.linalg.norm(np.array(current_position[j]) - np.array(c1[j])) < P["d1"]:
                 R_gaussian[j] = R_gaussian[j] - 1*P["dt"]
                 R_gaussian[j] = max(R_gaussian[j], P["R_gauss_min"])
             else:
                 R_gaussian[j]= R_gaussian[j] - 1*P["dt"]*(R_gaussian[j]-P["R_gaussianD"][j])
                             
             #equation (9)
-            if abs(np.linalg.norm(np.array(c1[j]) - np.array(c2[j]))) > P["d4"] and np.linalg.norm(np.array(current_position[j]) - np.array(c1[j])) < P["d3"]:# and distancemin[j] < size[j]+max(size)+1:
+            if abs(np.linalg.norm(np.array(c1[j]) - np.array(c2[j]))) > d4 and np.linalg.norm(np.array(current_position[j]) - np.array(c1[j])) < P["d3"]:# and distancemin[j] < size[j]+max(size)+1:
                 th[j] = min(th[j] + 1*P["dt"], math.pi/2.1)
             else:
                 th[j] = max(0, th[j] - 1*P["dt"] )
-            if th[j] == math.pi/2.1 and abs(np.linalg.norm(np.array(current_position[j]) - np.array(c1_no_rotation[j]))  >= np.linalg.norm(np.array(current_position[j]) - np.array(c1[j]))):
+            if th[j] == math.pi/2.1 and abs(np.linalg.norm(np.array(current_position[j]) - np.array(c1_no_rotation[j]))  > np.linalg.norm(np.array(current_position[j]) - np.array(c1[j]))):
                 th[j] = 0 
         
             angle     = math.atan2(goal[j][1] - current_position[j][1], goal[j][0] - current_position[j][0])
@@ -71,15 +81,16 @@ def simulate( h, P ):
             distance  = math.sqrt((goal[j][0] - current_position[j][0])**2 + (goal[j][1] - current_position[j][1])**2)
             Robots.destinations[j][0] = current_position[j][0] + distance * math.cos(new_angle)
             Robots.destinations[j][1] = current_position[j][1] + distance * math.sin(new_angle)
-        
+            end = time.time()
             #condition used for stop the simulation
-            if math.sqrt((current_position[j][0]-goal[j][0])**2 + (current_position[j][1]-goal[j][1])**2) < P["d2"]:
+
+            if math.sqrt((current_position[j][0]-goal[j][0])**2 + (current_position[j][1]-goal[j][1])**2) < d2+P["dx"]:
                 flag[j] = 1
             else:
                 flag[j] = 0
 
             if sum(flag) == P["N"] and j ==P["N"]-1:
-                 print("travel time:", round(step*dt,3), "(s).  max velocity:", round(tmp,3), "(m/s)" )
+                 print("travel time:", round(step*P["dt"],3), "(s).  max velocity:", round(tmp,3), "(m/s). max computational time")
 
             if P["flag_plot"] == 1:
                 plot_circle(current_position[j],P["size"][j],'blue')
