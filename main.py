da8977add250b1b1ca041af73a29706f50f623e6
Manuel Boldrer <95948066+manuelboldrer@users.noreply.github.com>
2023-09-07 11:16:53 +0200

    add video

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 from simulate import simulate
 from simulate import check_parameters
 
-N = 20                                 # Number of robots.
+N = 30                                 # Number of robots.
 
 parameters = {
         "R_circle": 0,                # If greater than 0, robots in a circle (radius of the circle).
@@ -11,16 +11,16 @@ parameters = {
         "Xplot": 5,                   # x-axis plot
         "Yplot": 5,                   # y-axis plot
         "radius": 1.5,                # Half of the sensing radius: dimension of the cells r_{s,i}=r_{s}.
-        "xlim": (0, 3),             # Random room dimensions on the X-axis.
-        "ylim": (0, 3),             # Random room dimensions on the Y-axis.
+        "xlim": (0, 2),             # Random room dimensions on the X-axis.
+        "ylim": (0, 2),             # Random room dimensions on the Y-axis.
         "N": N,                       # Number of robots.
         "num_steps": 3000,            # Number of simulation steps.
-        "dx": 0.1,                    # Space discretization. [It introduce an approximation. The lower the better, but it cost computation]
+        "dx": 0.05,                    # Space discretization. [It introduce an approximation. The lower the better, but it cost computation]
         "dt": 0.033,                  # Time discretization. [do not change]
         "d1": 0.2,                    # d1 eq. (8). [do not change]
         "d3": 0.2,                    # d3 eq. (9). [do not change]
         "R_gauss_min": 0.05,          # Minimum value for spreading factor rho. [do not change]
-        "R_gaussianD": [0.25]*N,       # Desired spreading factor \rho^D.
+        "R_gaussianD": [0.3]*N,       # Desired spreading factor \rho^D.
         "size": [random.uniform(0.15, 0.15) for _ in range(N)],        # Robots encumbrance \delta
         "k": [6]*N,                   # Control parameter k_p
         "flag_plot": 0,
